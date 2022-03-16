60e4d833bf9c5633381faf67c8ca8889d50dd3b1
praju <sawantprajakta84@gmail.com>
2022-03-16 11:53:20 +0530

    Added welcome Message

---
+++ src/com/java/bridgelabs/AddressBook.java
@@ -0,0 +1,8 @@
+package com.java.bridgelabs;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+        System.out.println("*-*-*-*-*-*- Welcome to Address Book -*-*-*-*-*-*-");
+    }
+}
